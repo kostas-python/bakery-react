9126c49bbe5dc5a7866fc7ab593c7959f93d8bb8
Kostas <diamantopoulos.kostas@hotmail.com>
2024-06-09 14:28:14 +0100

    changes

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,21 +1,21 @@
-import { LifebuoyIcon, NewspaperIcon, PhoneIcon } from '@heroicons/react/20/solid'
-import '/src/app/globals.css'
+import { ClockIcon,MapPinIcon, PhoneIcon } from '@heroicons/react/20/solid'
+
 
 const cards = [
   {
-    name: 'Sales',
+    name: 'Find Us',
     description: 'Consectetur vel non. Rerum ut consequatur nobis unde. Enim est quo corrupti consequatur. ',
-    icon: PhoneIcon,
+    icon: MapPinIcon,
   },
   {
-    name: 'Technical Support',
+    name: 'Hours',
     description: 'Quod possimus sit modi rerum exercitationem quaerat atque tenetur ullam.',
-    icon: LifebuoyIcon,
+    icon: ClockIcon,
   },
   {
-    name: 'Media Inquiries',
+    name: 'Call Us',
     description: 'Ratione et porro eligendi est sed ratione rerum itaque. Placeat accusantium impedit eum odit.',
-    icon: NewspaperIcon,
+    icon: PhoneIcon,
   },
 ]
 
@@ -29,22 +29,23 @@ export default function Example() {
         />
         <div className="px-6 py-24 sm:py-32 lg:px-8">
           <div className="mx-auto max-w-2xl text-center">
-            <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Support center</h2>
-            <p className="mt-6 text-lg leading-8 text-gray-300">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
+            <h2 className="text-4xl font-bold tracking-tight text-[#6B4721] sm:text-6xl">Contact Us</h2>
+            <p className="mt-6 text-lg leading-8 text-[#6B4721]">
+              "For any inquiries, orders, or feedback, please don't hesitate to reach out. Our friendly team is here to
+              assist you with all your baking needs. You can contact us by phone, email, or visit our bakery location. We
+              look forward to hearing from you and serving up delicious treats!"
             </p>
           </div>
           <div className="mx-auto mt-16 flex flex-wrap justify-center gap-4 sm:mt-20 lg:mx-0">
             {cards.map((card) => (
               <div
                 key={card.name}
-                className="max-w-xs flex gap-x-4 rounded-xl bg-white/5 p-4 ring-1 ring-inset ring-black/10"
+                className="max-w-xs flex gap-x-4 rounded-xl bg-white/5 p-4 ring-1 ring-inset ring-[#6B4721]"
               >
-                <card.icon className="h-6 w-4 flex-none text-indigo-400" aria-hidden="true" />
+                <card.icon className="h-6 w-4 flex-none text-[#C38D5E]" aria-hidden="true" />
                 <div className="text-sm leading-6">
                   <h3 className="font-semibold text-white text-sm">{card.name}</h3>
-                  <p className="mt-1 text-gray-300 text-sm">{card.description}</p>
+                  <p className="mt-1 text-[#C38D5E] text-sm">{card.description}</p>
                 </div>
               </div>
             ))}
@@ -53,5 +54,6 @@ export default function Example() {
       </div>
     );
   }
+  
   
   
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -15,17 +15,24 @@ export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-amber-600">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8" aria-label="Global">
+    <header className="bg-[#93763c]">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8"
+        aria-label="Global"
+      >
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
+            <span className="sr-only">Your Bakery</span>
             <img className="h-40 w-auto" src="bakerylogo1.png" alt="" />
           </a>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
           {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="text-xl font-semibold leading-6 text-orange-300">
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-2xl font-semibold leading-6 text-orange-900"
+            >
               {item.name}
             </a>
           ))}
@@ -36,7 +43,7 @@ export default function Example() {
         <div className="flex lg:hidden">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-[#6B1985]"
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
@@ -46,10 +53,10 @@ export default function Example() {
       </nav>
       <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-amber-600 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[#E3B04B] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-[#6B1985]/10">
           <div className="flex items-center gap-x-6">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+              <span className="sr-only">Your Bakery</span>
               <img
                 className="h-20 w-auto"
                 src="bakerylogo1.png"
@@ -58,13 +65,13 @@ export default function Example() {
             </a>
             <a
               href="#"
-              className="ml-auto rounded-md bg-orange-300 px-3 py-2 text-sm font-semibold text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
+              className="ml-auto rounded-md bg-[#8C1AAF] px-3 py-2 text-xl font-semibold text-white shadow-sm hover:bg-[#6B1985] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#6B1985]"
             >
               Menu
             </a>
             <button
               type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              className="-m-2.5 rounded-md p-2.5 text-[#6B1985]"
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
@@ -72,13 +79,13 @@ export default function Example() {
             </button>
           </div>
           <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
+            <div className="-my-6 divide-y divide-[#6B1985]/10">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-xl font-semibold leading-7 text-[#6B1985] hover:bg-[#F0D8A8]"
                   >
                     {item.name}
                   </a>
@@ -87,7 +94,7 @@ export default function Example() {
               <div className="py-6">
                 <a
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-xl font-semibold leading-7 text-[#6B1985] hover:bg-[#F0D8A8]"
                 >
                   Log in
                 </a>
@@ -99,3 +106,4 @@ export default function Example() {
     </header>
   )
 }
+
