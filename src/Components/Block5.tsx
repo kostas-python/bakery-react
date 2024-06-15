a904ab779e107aa7672c5d40f2faf755eb17d255
Kostas <diamantopoulos.kostas@hotmail.com>
2024-06-16 00:03:50 +0100

    image tag update

--- src/Components/Block5.tsx
+++ src/Components/Block5.tsx
@@ -1,20 +1,28 @@
 'use client'
 
 import React from 'react';
-import LazyLoad from 'react-lazyload';
+import Image from 'next/image';
+
 
 
 const ImageSection = () => {
   return (
-    <section className="relative bg-cover bg-center bg-[url('/hhh.jpg')]">
-      <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8 text-center">
-      <h2 className="text-6xl  text-center mt-8">"​Our freshly baked bread is the hero of our breakfast menu"</h2>
-        <LazyLoad>
-        <img src="/g-min.jpg" alt="" className="w-full h-[800px] mt-8 object-contain"/>
-        </LazyLoad>
+    <section className="relative bg-cover bg-center" style={{ backgroundImage: "url('/hhh.jpg')" }}>
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8 text-center text-black">
+        <h2 className="text-6xl mt-8">"Our freshly baked bread is the hero of our breakfast menu!"</h2>
+        <div className="relative w-full h-[800px] mt-8">
+          <Image
+            src="/g-min.jpg"
+            alt=""
+            layout="fill"
+            objectFit="contain"
+            loading="lazy"
+          />
+        </div>
       </div>
     </section>
   );
 };
 
 export default ImageSection;
+
