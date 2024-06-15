45269e6f60a1d9d9ef7fa4fdb5c6b55bb583bd45
Kostas <diamantopoulos.kostas@hotmail.com>
2024-06-16 00:30:00 +0100

    h2 correction

--- src/Components/Block5.tsx
+++ src/Components/Block5.tsx
@@ -9,7 +9,7 @@ const ImageSection = () => {
   return (
     <section className="relative bg-cover bg-center" style={{ backgroundImage: "url('/hhh.jpg')" }}>
       <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8 text-center text-black">
-        <h2 className="text-6xl mt-8">"Our freshly baked bread is the hero of our breakfast menu!"</h2>
+        <h2 className="text-6xl mt-8">&quot;Our freshly baked bread is the hero of our breakfast menu!&quot;</h2>
         <div className="relative w-full h-[800px] mt-8">
           <Image
             src="/g-min.jpg"
